a9b46de241a94941b4c8705a8b5d35e5694d0d91
ashray <ashray.malhotra.1994@gmail.com>
2015-05-29 10:10:52 +0530

    First commit

---
+++ violaJones.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import cv2
+import pdb
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
+
+img = cv2.imread('sampleFaceImage.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.equalizeHist(gray, gray)
+
+faces = face_cascade.detectMultiScale(gray, 1.1,2)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    #img = cv2.rectangle(img,(y,x),(y+h,x+w),(255,0,0),2)
+    #pdb.set_trace()
+
+    roi_gray = gray[y:y+h, x:x+w]
+    #pdb.set_trace()
+    #roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+    	pass
+        #cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+#pdb.set_trace()
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#pdb.set_trace()
