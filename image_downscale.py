0ecd4abbae7eb8146fd34a551f3e1bf6ece047b7
ashray <ashray.malhotra.1994@gmail.com>
2015-06-08 12:07:22 +0530

    Image Downscale now returns image and doesnt write it

--- image_downscale.py
+++ image_downscale.py
@@ -9,9 +9,10 @@ def image_downscale(input_image, max_dimension):
     else:
         dim = (int(((height * max_dimension) / width)), max_dimension)
     input_image = cv2.resize(input_image, dim)  # , interpolation = cv2.INTER_AREA)
+    #
+    # cv2.imshow('input_image', input_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    cv2.imshow('input_image', input_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    cv2.imwrite('downscaled_image.png', input_image)
+    # cv2.imwrite('downscaled_image.png', input_image)
+    return input_image
