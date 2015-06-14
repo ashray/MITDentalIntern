70ca31c6b02741b3edfd8fc21c6a926c89270588
ashray <ashray.malhotra.1994@gmail.com>
2015-06-15 00:15:22 +0530

    All miscellaneous functions in this file

---
+++ helperFunctions.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+import pdb
+
+def find_x(y, point1, point2):
+    x1 = point1[0]
+    y1 = point1[1]
+    x2 = point2[0]
+    y2 = point2[1]
+
+    # for a vertical line. Return either of the two points
+    if abs(x2-x1) < 2:
+        return x1
+    elif abs(y2-y1) == 0:
+        return "Error"
+    else:
+        m = (y2-y1)/(x2-x1)
+        x = (y-(y1-((y2-y1)/(x2-x1))*x1))/m
+        return x
+    # y = mx + c
