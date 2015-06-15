Fixed one case of symmetry percentage calculation, to fix second case

IntersectionPoint.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import pdb
+from helperFunctions import dot_product
 
 def Perpendicular(a) :
     b = np.empty_like(a)
@@ -12,12 +13,16 @@ def Perpendicular(a) :
 # line segment b given by endpoints b1, b2
 # return
 def LineSegmentIntersection(a1,a2, b1,b2):
-    da = a2-a1
-    db = b2-b1
-    dp = a1-b1
+    # da = int(math.ceil(math.sqrt((a2[0]-a1[0])*(a2[0]-a1[0]) + (a2[1]-a1[1])*(a2[1]-a1[1]))))
+    # db = int(math.ceil(math.sqrt((b2[0]-b1[0])*(b2[0]-b1[0]) + (b2[1]-b1[1])*(b2[1]-b1[1]))))
+    # dp = int(math.ceil(math.sqrt((a1[0]-b1[0])*(a1[0]-b1[0]) + (a1[1]-b1[1])*(a1[1]-b1[1]))))
+    da = a2 - a1
+    db = b2 - b1
+    dp = a1 - b1
     dap = Perpendicular(da)
-    denom = np.dot( dap, db)
-    num = np.dot( dap, dp )
+    # pdb.set_trace()
+    denom = dot_product( dap, db)
+    num = dot_product( dap, dp )
     return (num / denom.astype(float))*db + b1
 
 def lineMagnitude (x1, y1, x2, y2):

faceSymmetry.py
@@ -5,11 +5,13 @@ from image_downscale import image_downscale
 from violaJones import *
 from canny import *
 from symmetryMidpoints import symmetryMidpoints
+from symmetryCalculation import symmetryCalculationIntensity
 
 # Accept original image as input
-img = cv2.imread('sampleFaceImage.png')
+img = cv2.imread('./photo/sampleFaceImage10.png')
 img = image_downscale(img, 400)
 img_copy = img.copy()
+img_copy_2 = img.copy()
 gray = colGray(img)
 
 # midPoint = []
@@ -96,18 +98,26 @@ cv2.waitKey(0)
 # sub_image1 is the left side of the image
 # sub_image2 is the right side of the image
 # a has the boundary points of the face
-
+print 'Working till before perpen calc'
 [vx_perpen,vy_perpen] = Perpendicular([vx_temp,vy_temp])
 # cv2.line(img_copy,(intersection_x,intersection_y),(intersection_x+(100*vx_perpen),intersection_y+100*vy_perpen), (255, 224, 0), 6)
 # cv2.line(img_copy,(intersection_x,intersection_y),(intersection_x-(100*vx_perpen),intersection_y-100*vy_perpen), (255, 224, 0), 6)
+
+# intersection_x , intersection_y is the mid point of the two eyes. Perpendicular line passes through these points
 linePoint1 = [(intersection_x+(100*vx_perpen)),(intersection_y+100*vy_perpen)]
 linePoint2 = [intersection_x-(100*vx_perpen),(intersection_y-100*vy_perpen)]
 
 [leftIntersectionPoint, rightIntersectionPoint] = FaceSymmetryLineIntersection(a, linePoint1, linePoint2)
+# leftIntersectionPoint = np.array(leftIntersectionPoint)
+# rightIntersectionPoint = np.array(rightIntersectionPoint)
 cv2.line(img_copy_cropped, (leftIntersectionPoint[0],leftIntersectionPoint[1]), (leftIntersectionPoint[0],leftIntersectionPoint[1]), (0,255,0),10)
 cv2.line(img_copy_cropped, (rightIntersectionPoint[0],rightIntersectionPoint[1]), (rightIntersectionPoint[0],rightIntersectionPoint[1]), (0,255,0),10)
 cv2.imshow('img_new_cropped', img_copy_cropped)
 
+print 'Calling percent calc'
+
+left_percentage, right_percentage = symmetryCalculationIntensity(a,img_copy_2,leftIntersectionPoint,rightIntersectionPoint,xbf_temp,ybf_temp,vx_temp,vy_temp)
+print 'Left % = ',left_percentage,' Right % = ',right_percentage
 #percentageDifference = math.fabs(sum_image1[0] - sum_image2[0]) / max(sum_image1[0], sum_image2[0])
 #print "Percentage asymmetry ", percentageDifference * 100
 

symmetryCalculation.py
@@ -6,10 +6,35 @@ from helperFunctions import find_x
 
 #Finds symmetry percentage considering face boundary and central symmetry line
 # We need the input image, the face curve points(faceCurve) and the two intersection points(of the eye line)
-def symmetryCalculationIntensity(face_boundary_points,input_img, eye_line_point1,eye_line_point2, x_symmetry, y_symmetry, vx_symmetry, vy_symmetry):
+def symmetryCalculationIntensity(face_boundary_points,input_img, eye_line_point1_woNP,eye_line_point2_woNP, x_symmetry, y_symmetry, vx_symmetry, vy_symmetry):
     # We first need to get the points of the symmetry line
-    symmetry_point1 = [(x_symmetry - 200*vx_symmetry),(y_symmetry - 200*vy_symmetry)]
-    symmetry_point2 = [(x_symmetry + 200*vx_symmetry),(y_symmetry + 200*vy_symmetry)]
+    symmetry_point1_woNP = [(x_symmetry - 200*vx_symmetry),(y_symmetry - 200*vy_symmetry)]
+    symmetry_point2_woNP = [(x_symmetry + 200*vx_symmetry),(y_symmetry + 200*vy_symmetry)]
+
+    # symmetry_point1_woNP = x_symmetry - 200*vx_symmetry,y_symmetry - 200*vy_symmetry
+    # symmetry_point2_woNP = x_symmetry + 200*vx_symmetry,y_symmetry + 200*vy_symmetry
+
+    # symmetry_point1 = np.array([(x_symmetry - 200*vx_symmetry),(y_symmetry - 200*vy_symmetry)])
+    # symmetry_point2 = np.array([(x_symmetry + 200*vx_symmetry),(y_symmetry + 200*vy_symmetry)])
+
+    # symmetry_point1 = np.array(symmetry_point1_woNP)
+    # symmetry_point2 = np.array(symmetry_point2_woNP)
+
+    symmetry_point1 = symmetry_point1_woNP
+    symmetry_point2 = symmetry_point2_woNP
+
+    # eye_line_point1 = np.array([(eye_line_point1[0]),(eye_line_point1[1])])
+    # eye_line_point2 = np.array([(eye_line_point2[0]),(eye_line_point2[1])])
+
+    # eye_line_point1 = np.array(eye_line_point1_woNP)
+    # eye_line_point2 = np.array(eye_line_point2_woNP)
+    #
+    # # so now a hack - which also didn't work !
+    # eye_line_point1 = np.transpose(eye_line_point1)
+    # eye_line_point2 = np.transpose(eye_line_point2)
+
+    eye_line_point1 = eye_line_point1_woNP
+    eye_line_point2 = eye_line_point2_woNP
 
     # we come down in y axis
     row, col, depth = input_img.shape
@@ -18,7 +43,7 @@ def symmetryCalculationIntensity(face_boundary_points,input_img, eye_line_point1
     else:
         pass
     # theta = 0
-
+    # pdb.set_trace()
     # We know that eye_line_point1 is going to be on the left side of the face
     points_count = len(face_boundary_points[0,:])
     left_boundary = face_boundary_points[:,0:((points_count/2)-1)]
@@ -26,28 +51,84 @@ def symmetryCalculationIntensity(face_boundary_points,input_img, eye_line_point1
 
     # We need to scan all the points on the left boundary which have y value greater than eye_line_point1
     sum = 0
+    symmetry_point1 = np.asarray(symmetry_point1)
+    symmetry_point2 = np.asarray(symmetry_point2)
+    symmetry_point1 = symmetry_point1.astype(int)
+    symmetry_point2 = symmetry_point2.astype(int)
+
+    # symmetry_perpendicular_intersection = LineSegmentIntersection([int(symmetry_point1[0]), int(symmetry_point1[1])], [int(symmetry_point2[0]), int(symmetry_point2[1])], eye_line_point1, eye_line_point2)
+    eye_line_point1 = np.asarray(eye_line_point1)
+    eye_line_point2 = np.asarray(eye_line_point2)
+    eye_line_point1 = eye_line_point1.reshape((2,1))
+    eye_line_point2 = eye_line_point2.reshape((2,1))
+    eye_line_point1 = eye_line_point1.astype(int)
+    eye_line_point2 = eye_line_point2.astype(int)
+    # pdb.set_trace()
     symmetry_perpendicular_intersection = LineSegmentIntersection(symmetry_point1, symmetry_point2, eye_line_point1, eye_line_point2)
+
+    symmetry_perpendicular_intersection = np.asarray(symmetry_perpendicular_intersection)
+    symmetry_perpendicular_intersection = symmetry_perpendicular_intersection.astype(int)
 
     sum_left = 0
     sum_right = 0
     for i in range(0, (points_count/2) - 1):
         # The above loop will run if eye_line_point1[1]< eye_line_point2[1]
-        if eye_line_point1[1]<eye_line_point2[1]:
+        if (eye_line_point1[1]<eye_line_point2[1]) or (eye_line_point1[1] == eye_line_point2[1]):
             if face_boundary_points[1, i] < eye_line_point1[1]:
+                print 'pass ',i
                 pass
             elif (face_boundary_points[1,i] < symmetry_perpendicular_intersection[1]) and (eye_line_point1[1] < face_boundary_points[1,i]):
                 xf = find_x(face_boundary_points[1],eye_line_point1, eye_line_point2)
                 sum_left = sum_left + np.sum(input_img[face_boundary_points[0,i]:xf, face_boundary_points[1,i]])
+                print 'cond 1'
             elif ((symmetry_perpendicular_intersection[1]<face_boundary_points[1,i]) and (face_boundary_points[1,i]<eye_line_point2[1])):
                 xf = find_x(face_boundary_points[1], eye_line_point1, eye_line_point2)
                 xg = find_x(face_boundary_points[1], symmetry_point1, symmetry_point2)
                 sum_left = sum_left + np.sum(input_img[face_boundary_points[0,i]:xg, face_boundary_points[1,i]])
                 sum_right = sum_right + np.sum(input_img[xg:xf, face_boundary_points[1,i]])
-            elif face_boundary_points[1]>eye_line_point2[1]:
+                print 'cond 2'
+
+            elif face_boundary_points[1,i]>eye_line_point2[1]:
                 xg = find_x(face_boundary_points[1], symmetry_point1, symmetry_point2)
                 sum_left = sum_left + np.sum(input_img[face_boundary_points[0,i]:xg, face_boundary_points[1,i]])
-                sum_right = sum_right + np.sum(input_img[xg:face_boundary_points[1,i], face_boundary_points[1,i]])
+                sum_right = sum_right + np.sum(input_img[xg:right_boundary[0,i], face_boundary_points[1,i]])
+                # sum_right = sum_right + np.sum(input_img[xg:face_boundary_points[1,i], face_boundary_points[1,i]])
+                print 'cond 3'
+
+            # total_sum = sum_left + sum_right
+    #         # pdb.set_trace()set_trace
+    # left_percentage = (sum_left/total_sum)*100
+    # right_percentage = (sum_right/total_sum)*100
+
+
 
         # For the case where eye_line_point1[1]>=eye_line_point2[1]:
-        else
+        else:
+            print 'Case 2 - left tilted face'
+            if face_boundary_points[1, i] < eye_line_point2[1]:
+                pass
+            elif (face_boundary_points[1,i] < symmetry_perpendicular_intersection[1]) and (eye_line_point2[1] < face_boundary_points[1,i]) :
+                xf = find_x(face_boundary_points[1], eye_line_point1, eye_line_point2)
+                sum_right = sum_right + np.sum(input_img[xf:right_boundary[0,i], face_boundary_points[1,i]])
+                #
+                # xg = find_x(face_boundary_points[1], symmetry_point1, symmetry_point2)
+                # sum_left = sum_left + np.sum(input_img[face_boundary_points[0,i]:xg, face_boundary_points[1,i]])
+                # sum_right = sum_right + np.sum(input_img[xg:xf, face_boundary_points[1,i]])
+            elif ((symmetry_perpendicular_intersection[1]<face_boundary_points[1,i]) and (face_boundary_points[1,i]<eye_line_point1[1])):
+                xf = find_x(face_boundary_points[1],eye_line_point1, eye_line_point2)
+                xg = find_x(face_boundary_points[1], symmetry_point1, symmetry_point2)
+                sum_left = sum_left + np.sum(input_img[xf:xg, face_boundary_points[1,i]])
+                sum_right = sum_right + np.sum(input_img[xg:right_boundary[0,i], face_boundary_points[1,i]])
+            elif face_boundary_points[1,i]>eye_line_point1[1]:
+                xg = find_x(face_boundary_points[1], symmetry_point1, symmetry_point2)
+                # pdb.set_trace()
+                sum_left = sum_left + np.sum(input_img[face_boundary_points[0,i]:xg, face_boundary_points[1,i]])
+                sum_right = sum_right + np.sum(input_img[xg:right_boundary[0,i], face_boundary_points[1,i]])
+
+    total_sum = sum_left + sum_right
+    # pdb.set_trace()
+    left_percentage = (sum_left/total_sum)*100
+    right_percentage = (sum_right/total_sum)*100
+
+    return left_percentage, right_percentage
 